6a580730e12a051936bdc2b320f3bb01c5cc5c16
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2018-03-01 16:28:22 +0200

    First Copy of The Game
    - Added Power Module
    - Added UDP

---
+++ EEG Game/Assets/Scripts/Managers/PowerManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PowerManager : MonoBehaviour
+{
+    Text text;
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+    }
+
+    void Update ()
+    {
+        text.text = "Power: " + UDP.power.ToString();
+    }
+}
---
+++ EEG Game/Assets/Scripts/Player/UDP.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System;
+
+public class UDP : MonoBehaviour
+{
+    Thread thread;
+    static readonly object lockObject = new object();
+    bool precessData = false;
+    public static int power;
+
+    void Start()
+    {
+        power = 1;
+        thread = new Thread(new ThreadStart(ThreadMethod));
+        thread.Start();
+    }
+
+    void Update()
+    {
+        if (precessData)
+        {
+            precessData = false;
+            //Process received data
+            Debug.Log("Received: " + power);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        //udp.Close();
+        thread.Abort();
+    }
+
+    private void ThreadMethod()
+    {
+        UdpClient udp = new UdpClient(12345);
+        while (true)
+        {
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            try
+            {
+                Byte[] receiveBytes = udp.Receive(ref RemoteIpEndPoint);
+                power = Int32.Parse(Encoding.ASCII.GetString(receiveBytes));
+                Debug.Log(power);
+                //Done, notify the Update function
+                precessData = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
