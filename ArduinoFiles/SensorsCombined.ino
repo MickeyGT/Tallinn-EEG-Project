d259861bc1e5e467044a8f376fb04890296af918
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2018-03-12 12:23:45 +0200

    Create SensorsCombined.ino

---
+++ ArduinoFiles/SensorsCombined.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+
+const int GSR=A0;
+int sensorValue=0;
+int gsr_average=0;
+
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.println("Heart rate sensor and GSR sensor:");
+    Wire.begin();
+}
+void loop()
+{
+    Wire.requestFrom(0xA0 >> 1, 1);    // request 1 bytes from slave device
+    while(Wire.available())  // slave may send less than requested
+    {         
+        unsigned char c = Wire.read();   // receive heart rate value (a byte)
+        Serial.println(c, DEC);         // print heart rate value
+    }
+
+    long sum=0;
+    for(int i=0;i<10;i++)           //Average the 10 measurements to remove the glitch
+    {
+      sensorValue=analogRead(GSR);
+      sum += sensorValue;
+      delay(5);
+    }
+   gsr_average = sum/10;
+   Serial.println("GSR measurement: "+gsr_average);
+    
+    delay(500);
+}
