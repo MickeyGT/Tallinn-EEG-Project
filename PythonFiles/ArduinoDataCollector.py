dd06f514c99d0d23a4be74aff88a6ee3b2471aa3
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2018-03-12 12:54:05 +0200

    Python code now also sends data via UDP

--- PythonFiles/ArduinoDataCollector.py
+++ PythonFiles/ArduinoDataCollector.py
@@ -1,6 +1,11 @@
 import serial
+import socket
 arduino = serial.Serial('COM4', 9600, timeout=.1)
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 while True:
 	data = arduino.readline()[:-2] #the last bit gets rid of the new-line chars
 	if data:
 		print(data)
+		sock.sendto(data,(UDP_IP,UDP_PORT))
