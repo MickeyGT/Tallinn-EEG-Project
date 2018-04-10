056cf792126e956cbe22a2d81df0b779109602ea
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2018-04-10 15:34:09 +0300

    Drawn face from Affectiva
    - Webcam needs to be set manually.

---
+++ Affectiva to Face/Assets/AffectivaProcess.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Affdex;
+using UnityEditor;
+using System.IO;
+
+public class AffectivaProcess : ImageResultsListener
+{
+    private FeaturePoint[] featurePointsList;
+    FeaturePoint fp;
+    public override void onFaceFound(float timestamp, int faceId)
+    {
+        Debug.Log("Found the face");
+    }
+
+    public override void onFaceLost(float timestamp, int faceId)
+    {
+        Debug.Log("Lost the face");
+    }
+
+    public override void onImageResults(Dictionary<int, Face> faces)
+    {
+        Debug.Log("Got face results");
+
+        foreach (KeyValuePair<int, Face> pair in faces)
+        {
+            int FaceId = pair.Key;  // The Face Unique Id.
+            Face face = pair.Value;
+            featurePointsList = face.FeaturePoints;
+            foreach(FeaturePoint fp in featurePointsList)
+            {
+                DrawPoints.changeSphere(fp.id, fp.x, fp.y);
+            }
+        }
+    }
+}
---
+++ Affectiva to Face/Assets/DrawPoints.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using System.Collections;
+using Affdex;
+
+public class DrawPoints : MonoBehaviour
+{
+    Transform mainCamera;
+    CameraInput cameraInput;
+    string cameraName = "USB 2.0 Webcam Device";
+    string currentCameraName = "";
+    public static GameObject[] spheres = new GameObject[34];
+    private Vector3 offset;
+
+    private void Start()
+    {
+        for (int i = 0; i <= 33; i++)
+        {
+            spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            spheres[i].transform.localScale = new Vector3(10, 10, 1);
+        }
+        offset = this.transform.position - spheres[11].transform.position;
+    }
+
+    public static void changeSphere(int id, float x,float y)
+    {
+        spheres[id].transform.position = new Vector3(x, y, 0F);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (currentCameraName != cameraName)
+        {
+            cameraInput.SelectCamera(true, cameraName);
+            currentCameraName = cameraName;
+        }
+
+    }
+
+    void LateUpdate()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        this.transform.position = spheres[11].transform.position + offset;
+    }
+
+    void Awake()
+    {
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        cameraInput = mainCamera.GetComponent<CameraInput>();
+    }
+}
