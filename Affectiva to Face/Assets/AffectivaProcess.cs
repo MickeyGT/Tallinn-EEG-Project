cffe9e66cb04fc5996152610ce53c019a2803385
Turcu Gabriel <MickeyGT@users.noreply.github.com>
2018-04-11 18:13:51 +0300

    Added Tobii Eye Tracking + Eye and pupil

--- Affectiva to Face/Assets/AffectivaProcess.cs
+++ Affectiva to Face/Assets/AffectivaProcess.cs
@@ -32,6 +32,8 @@ public class AffectivaProcess : ImageResultsListener
             {
                 DrawPoints.changeSphere(fp.id, fp.x, fp.y);
             }
+            DrawPoints.changeLeftEye((featurePointsList[18].x+ featurePointsList[19].x + featurePointsList[32].x + featurePointsList[33].x)/4, (featurePointsList[32].y + featurePointsList[33].y + featurePointsList[18].y + featurePointsList[19].y)/4);
+            DrawPoints.changeRightEye((featurePointsList[16].x + featurePointsList[17].x + featurePointsList[30].x + featurePointsList[31].x)/4, (featurePointsList[16].y + featurePointsList[17].y + featurePointsList[30].y + featurePointsList[31].y)/4);
         }
     }
 }
--- Affectiva to Face/Assets/DrawPoints.cs
+++ Affectiva to Face/Assets/DrawPoints.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine;
 using System.Collections;
+using Tobii.Gaming;
 using Affdex;
 
 public class DrawPoints : MonoBehaviour
@@ -13,20 +14,74 @@ public class DrawPoints : MonoBehaviour
     string currentCameraName = "";
     public static GameObject[] spheres = new GameObject[34];
     private Vector3 offset;
+    private static GameObject face,leftEye,rightEye,leftPupil,rightPupil;
 
     private void Start()
     {
+        Color32 color = new Color32(255, 187, 153,255);
+        //color.a = 0;
+        /*
+        face = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        face.transform.localScale = new Vector3(250, 400, 1);
+        face.transform.position = this.transform.position;
+        face.GetComponent<Renderer>().material.color = color;
+        */
+        leftEye = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        leftEye.transform.localScale = new Vector3(0.5f, 0.5f, 1);
+        leftEye.transform.position = this.transform.position;
+        leftEye.GetComponent<Renderer>().material.color = Color.black;
+
+        rightEye = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        rightEye.transform.localScale = new Vector3(0.5f, 0.5f, 1);
+        rightEye.transform.position = this.transform.position;
+        rightEye.GetComponent<Renderer>().material.color = Color.black;
+
+        rightPupil = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        rightPupil.transform.localScale = new Vector3(0.2f, 0.2f, 1);
+        rightPupil.transform.position = this.transform.position;
+        rightPupil.GetComponent<Renderer>().material.color = Color.blue;
+
+        leftPupil = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        leftPupil.transform.localScale = new Vector3(0.2f, 0.2f, 1);
+        leftPupil.transform.position = this.transform.position;
+        leftPupil.GetComponent<Renderer>().material.color = Color.blue;
+
         for (int i = 0; i <= 33; i++)
         {
             spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            spheres[i].transform.localScale = new Vector3(10, 10, 1);
+            //spheres[i].transform.localScale = new Vector3(10, 10, 1);
+            spheres[i].transform.localScale = new Vector3(0.2f, 0.2f, 1);
         }
         offset = this.transform.position - spheres[11].transform.position;
     }
 
     public static void changeSphere(int id, float x,float y)
     {
-        spheres[id].transform.position = new Vector3(x, y, 0F);
+        spheres[id].transform.position = new Vector3(x/30, y/30, 0F);
+    }
+
+    public static void changeLeftEye(float x, float y)
+    {
+        leftEye.transform.position = new Vector3(x/30,y/30,+10);
+        GazePoint gazePoint = TobiiAPI.GetGazePoint();
+        if (gazePoint.IsValid)
+        {
+            Vector2 gazePosition = gazePoint.Screen;
+            Vector2 roundedSampleInput = new Vector2(Mathf.RoundToInt(gazePosition.x), Mathf.RoundToInt(gazePosition.y));
+            rightPupil.transform.position = new Vector3(rightEye.transform.position.x+(roundedSampleInput.x-960)/960 * -0.2f, rightEye.transform.position.y + (roundedSampleInput.y - 540) / 540 * -0.2f, 0);
+        }
+    }
+
+    public static void changeRightEye(float x, float y)
+    {
+        rightEye.transform.position = new Vector3(x/30, y/30, +10);
+        GazePoint gazePoint = TobiiAPI.GetGazePoint();
+        if (gazePoint.IsValid)
+        {
+            Vector2 gazePosition = gazePoint.Screen;
+            Vector2 roundedSampleInput = new Vector2(Mathf.RoundToInt(gazePosition.x), Mathf.RoundToInt(gazePosition.y));
+            leftPupil.transform.position = new Vector3(leftEye.transform.position.x + (roundedSampleInput.x - 960) / 960 * -0.2f, leftEye.transform.position.y + (roundedSampleInput.y - 540) / 540 * -0.2f, 0);
+        }
     }
 
     // Update is called once per frame
@@ -44,7 +99,9 @@ public class DrawPoints : MonoBehaviour
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        this.transform.position = spheres[11].transform.position + offset;
+        //this.transform.position = spheres[11].transform.position + offset;
+        //face.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, 0F);
+
     }
 
     void Awake()
